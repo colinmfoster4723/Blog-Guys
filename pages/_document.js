b41599c0581cefc05aae114a1a375c7903289bb5
colinmfoster4723 <colin.foster4723@gmail.com>
2022-09-24 21:09:26 -0500

    orientation lock for mobile

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
           content="Blob Guys V2 NFT Collection: guys derived from blobs"
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import { useEffect } from "react";
 import Controls from "../components/Controls";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -5,8 +7,15 @@ import Preview from "../components/Preview";
 import blobguys from "../web3/blobguys";
 
 export default function Home(props) {
+  useEffect(() => {
+    screen.orientation.lock("natural");
+  });
   return (
     <div className="pgContain">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Blob Guys</title>
+      </Head>
       <Header />
       <div className="contain">
         <Preview gif={"/blobguys_prev.gif"} />
